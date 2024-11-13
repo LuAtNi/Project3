e2f0de4372813d4e1aec3cabf56139da8f3b5a52
Athena Ballensky <athenaballenskyschool@gmail.com>
2024-11-13 10:34:53 -0800

    Added reheap method

--- Project3/MaxHeap.java
+++ Project3/MaxHeap.java
@@ -62,10 +62,6 @@ public final class MaxHeap<T extends Comparable<? super T>>
       return root;
    } // end removeMax
 
-   private void reheap(int rootIndex){
-      //reheap method
-   }
-
    public T getMax()
    {
 		checkIntegrity();
@@ -97,5 +93,31 @@ public final class MaxHeap<T extends Comparable<? super T>>
    } // end clear
    
 // Private methods
-// . . .
+
+   private void reheap(int rootIndex){
+      boolean done = false;
+      T orphan = heap[rootIndex];
+      int leftChildIndex = 2 * rootIndex;
+
+      while (!done && (leftChildIndex <= lastIndex))
+      {
+         int largerChildIndex = leftChildIndex; //Assume larger
+         int rightChildIndex = leftChildIndex + 1;
+         if ((rightChildIndex <= lastIndex) &&
+               heap[rightChildIndex].compareTo(heap[largerChildIndex]) > 0)
+         {
+            largerChildIndex = rightChildIndex;
+         }
+         if (orphan.compareTo(heap[largerChildIndex]) < 0)
+         {
+            heap[rootIndex] = heap[largerChildIndex];
+            rootIndex = largerChildIndex;
+            leftChildIndex = 2 * rootIndex;
+         }
+         else
+            done = true;
+      } // end while
+      heap[rootIndex] = orphan;
+   } // end reheap
+
 } // end MaxHeap
