9e81b689092c445220d0c820908e9daa28b1dac1
nikhithavasiraju <nnvasiraju@gmail.com>
2024-11-13 10:37:40 -0800

    Started creating the output file

---
+++ Project3/HeapOutput.java
@@ -0,0 +1,12 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class HeapOutput {
+    public static void main(String[] args){
+        try{
+            File file = new File("data_sorted.txt");
+        }
+    }
+    
+}
