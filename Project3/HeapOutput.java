63b63104016c92777c7e38cdc4153f8754a5389b
nikhithavasiraju <nnvasiraju@gmail.com>
2024-11-19 18:27:15 -0800

    Imported files to use in main class

--- Project3/HeapOutput.java
+++ Project3/HeapOutput.java
@@ -1,6 +1,10 @@
+
+import java.util.Scanner;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class HeapOutput {
     public static void main(String[] args){
